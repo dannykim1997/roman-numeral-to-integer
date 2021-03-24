0a6648979d8e7b89cbbd955f23d48b508a73c5d7
github username <github email address>
2021-03-24 13:59:32 -0700

    done

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,5 +1,28 @@
 function romanNumeral(string) {
-  // type your code here
+  let value = 0;
+    const order = ["M", "D", "C", "L", "X", "V", "I"];
+    const key = {
+        M: 1000,
+        D: 500,
+        C: 100,
+        L: 50,
+        X: 10,
+        V: 5,
+        I: 1,
+    };
+
+    for (let i = 0; i < string.length; i++) {
+        value += key[string.charAt(i)];
+        if (i !== 0) {
+            if (
+                string.charAt(i - 1) === order[order.indexOf(string.charAt(i)) + 1] ||
+                string.charAt(i - 1) === order[order.indexOf(string.charAt(i)) + 2]
+            ) {
+                value -= key[string.charAt(i - 1)] * 2;
+            }
+        }
+    }
+    return value;
 }
 
 if (require.main === module) {
